c4085d9299dbd8ad3c4a30811612fde5c291f9f7
Oscar Aceves <itan040@hotmail.com>
2018-01-27 06:03:28 -0600

    Add colors by player for each respective trail

--- Assets/Josue/PlayerLogic.cs
+++ Assets/Josue/PlayerLogic.cs
@@ -9,9 +9,11 @@ public enum MOVE_SET
     MOVE_SET_MAX
 }
 
+
 public class PlayerLogic : MonoBehaviour
 {
     public PLAYER m_PlayerNumber;
+    public GameObject TrailPrefab; 
 
     MOVE_SET m_ActiveMoveset;
     float m_Energy;
@@ -21,6 +23,7 @@ public class PlayerLogic : MonoBehaviour
     void Start()
     {
         Live();
+        SetColor();
     }
 
     void Live()
@@ -34,5 +37,26 @@ public class PlayerLogic : MonoBehaviour
     {
         m_Active = false;
         GameState.GlobalGameState.PlayerKilled(m_PlayerNumber);
+    }
+
+    void SetColor() {
+        var player = gameObject.GetComponent<PlayerLogic>().m_PlayerNumber;
+        Debug.Log(player.ToString());
+        switch (player) {
+            case PLAYER.PLAYER_1:
+                TrailPrefab.GetComponent<TrailRenderer>().materials[0].color = Color.red;
+                break;
+            case PLAYER.PLAYER_2:
+                TrailPrefab.GetComponent<TrailRenderer>().materials[0].color = Color.blue;
+                break;
+            case PLAYER.PLAYER_3:
+                TrailPrefab.GetComponent<TrailRenderer>().materials[0].color = Color.yellow;
+                break;
+            case PLAYER.PLAYER_4:
+                TrailPrefab.GetComponent<TrailRenderer>().materials[0].color = Color.green;
+                break;
+            default:
+                break;
+        }
     }
 }
