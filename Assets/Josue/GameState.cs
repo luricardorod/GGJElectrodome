5d9b2bbb5eef8d541a538b403cf2fe46f0a48577
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2018-01-28 06:53:41 -0600

    +added good shit

--- Assets/Josue/GameState.cs
+++ Assets/Josue/GameState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public enum GAME_STATE
 {
-    MENU,
+    MENU = 0,
     PRE_ROUND,
     PLAYING,
     PLAYER_DEATH,
@@ -12,9 +12,15 @@ public enum GAME_STATE
     MATCH_OVER
 };
 
+public enum GAME_TYPE
+{
+   DEATH_MATCH = 0,
+   PARTY
+};
+
 public enum PLAYER
 {
-    PLAYER_1,
+    PLAYER_1 = 0,
     PLAYER_2,
     PLAYER_3,
     PLAYER_4,
@@ -23,15 +29,16 @@ public enum PLAYER
 
 public class GameState : MonoBehaviour
 {
+    private int LastLevel=-1;
     public static GameState GlobalGameState;
-
+    public int MaxScore=5;
     public GAME_STATE m_State;
-
+    private float fTimeToWait;
     public GameObject playerPrefab;
     GameObject[] m_PlayerPool;
-
-    uint[] m_PlayerScore;
-    bool[] m_PlayerIsAlive;
+    private GameObject Level;
+    public uint[] m_PlayerScore;
+    public bool[] m_PlayerIsAlive;
 
     uint m_StartingPlayers;
     uint m_NumberOfPlayersAlive;
@@ -40,38 +47,106 @@ public class GameState : MonoBehaviour
     {
         DontDestroyOnLoad(this);
         GlobalGameState = this;
-
+        ///
         m_PlayerScore = new uint[(int)PLAYER.PLAYER_MAX];
         m_PlayerIsAlive = new bool[(int)PLAYER.PLAYER_MAX];
-
         m_PlayerPool = new GameObject[(int)PLAYER.PLAYER_MAX];
-
+        ///
         for (int i = 0; i < (int)PLAYER.PLAYER_MAX; ++i)
         {
-            m_PlayerPool[i] = Instantiate<GameObject>(playerPrefab, new Vector3(0, 5, i*2), Quaternion.identity);
+            m_PlayerPool[i] = Instantiate<GameObject>(playerPrefab);
             m_PlayerPool[i].GetComponent<PlayerLogic>().m_PlayerNumber = (PLAYER)i;
+            m_PlayerPool[i].gameObject.SetActive(false);
         }
+
+        //TODO: remove
+        InitMatch(PLAYER.PLAYER_MAX);
     }
 
     public void InitMatch(PLAYER playerNumber)
     {
-        m_StartingPlayers = (uint)playerNumber + 1;
-
+        m_StartingPlayers = (uint)playerNumber;
+        for (int i = 0; i < 4; ++i) m_PlayerScore[i] = 0;
         InitRound();
     }
 
     public void InitRound()
     {
-        m_NumberOfPlayersAlive = m_StartingPlayers;
-        m_State = GAME_STATE.PRE_ROUND;
+       m_NumberOfPlayersAlive = m_StartingPlayers;
+       m_State = GAME_STATE.PRE_ROUND;
+       for (int i = 0; i < 4; ++i) m_PlayerIsAlive[i] = true;
+       int randlevel = Random.Range(0, GetComponent<LevelLoader>().Levels.Count-1);
+       while (randlevel == LastLevel)
+       {
+           randlevel = Random.Range(0, GetComponent<LevelLoader>().Levels.Count - 1);
+       }
+LastLevel=randLevel;
+       GetComponent<LevelLoader>().SetLevel(randlevel);
+       Level = GameObject.FindGameObjectWithTag("Level");
+       int spawnindex = 0;
+       foreach (GameObject Player in m_PlayerPool)
+       {
+           Player.transform.position = Level.transform.GetChild(spawnindex).transform.position;
+           Player.transform.rotation = Level.transform.GetChild(spawnindex).transform.rotation;
+           Player.transform.localScale = Level.transform.GetChild(spawnindex).transform.localScale;
+           Player.gameObject.SetActive(true);
+           Player.gameObject.GetComponent<PlayerLogic>().enabled = false;
+           ++spawnindex;
+       }
+       Invoke("WaitToMove",3);
+    }
 
-        for (int i = 0; i < m_NumberOfPlayersAlive; ++i)
-            m_PlayerIsAlive[i] = true;
+    void WaitToMove()
+    {
+      foreach (GameObject Player in m_PlayerPool)
+      {
+          Player.gameObject.GetComponent<PlayerLogic>().enabled = true;
+      }
+        m_State = GAME_STATE.PLAYING;
+    }
+
+    void Update()
+    {
+        if (m_State == GAME_STATE.PLAYING)
+        {
+            CheckForRoundEnd();
+        }
+        else if (m_State == GAME_STATE.ROUND_OVER)
+        {
+            foreach(uint Score in m_PlayerScore)
+            {
+                if(Score>=MaxScore)
+                {
+                    m_State= GAME_STATE.MATCH_OVER;
+                }
+            }
+
+        }
+        else if (m_State == GAME_STATE.MATCH_OVER)
+        {
+            m_State = GAME_STATE.MENU;
+        }
     }
 
     void CheckForRoundEnd()
     {
-        //If there is just one player left.
+        for(int index=0;index<4;++index)
+        {
+            /* if(!m_PlayerPool[index].GetComponent<PlayerInfo>().isAlive)
+             {
+                 if (m_PlayerPool[index].GetComponent<PlayerInfo>().Lives>0)
+                 {
+                     m_PlayerPool[index].transform.GetChild(0).GetComponent<Rigidbody>().isKinematic = true;
+                     m_PlayerPool[index].transform.position = Level.transform.GetChild(index).transform.position;
+                     m_PlayerPool[index].transform.rotation = Level.transform.GetChild(index).transform.rotation;
+                     m_PlayerPool[index].transform.localScale = Level.transform.GetChild(index).transform.localScale;
+                     --m_PlayerPool[index].GetComponent<PlayerInfo>().Lives;
+                     m_PlayerPool[index].transform.GetChild(0).GetComponent<Rigidbody>().isKinematic = false;
+                 }
+        }*/
+     }
+
+        //Fuck you tho
         if (m_NumberOfPlayersAlive == 1)
         {
             //Look for the player still alive.
@@ -90,6 +165,7 @@ public class GameState : MonoBehaviour
             }
         }
     }
+
 
     /// <summary>
     /// A function to call when a player gets killed. Receives the player killed.
