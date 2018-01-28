224b2e89b9defb1a09892d077468def9c6fdfec9
CarlosE <carlos.caro77@hotmail.com>
2018-01-28 14:08:01 -0600

    MenuAnim
    Ya se ha implementado la animación de menú.

---
+++ ASsets/Intro/Scene/Script/Intro.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour {
+    public Animation anim;
+    private Animator animater;
+
+    private float Timer;
+    private void Start()
+    {
+        animater = GetComponent<Animator>();
+        animater.Play("MenuAnim");
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("");   
+    }
+}
